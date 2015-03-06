16feae21bbd976750d61d1419b742962453dbf45
Alexander Ernfridsson <aleer760@li0-5.ida.liu.se>
2015-03-06 15:13:16 +0100

    lect uppd

--- RouterNode.java
+++ RouterNode.java
@@ -5,6 +5,7 @@ public class RouterNode {
   private GuiTextArea myGUI;
   private RouterSimulator sim;
   private int[][] costs = new int[RouterSimulator.NUM_NODES][RouterSimulator.NUM_NODES];
+    private int[] myCost = new int[RouterSimulator.NUM_NODES];
 
   //--------------------------------------------------
   public RouterNode(int ID, RouterSimulator sim, int[] costs) {
@@ -19,55 +20,76 @@ public class RouterNode {
 	}
     }
 
-    // Copy start costs for node to table
+    // Copy start costs for node to our table
+    System.arraycopy(costs, 0, this.myCost, 0, RouterSimulator.NUM_NODES);
     System.arraycopy(costs, 0, this.costs[myID], 0, RouterSimulator.NUM_NODES);
-    
-    // Send nodes dist-vector to neighbors.
-    for( int nodeID = 0 ; nodeID < RouterSimulator.NUM_NODES; ++nodeID ){
-	if( myID != nodeID ){
-	    RouterPacket newPkt = new RouterPacket(this.myID, nodeID, this.costs[myID]);
-	    sendUpdate(newPkt); 
-	}
-    }
+
+    sendAllUpdate();
     
   }
 
-  // Called by the simulator (executed) when a node receives an update from one of its neighbors
-  public void recvUpdate(RouterPacket pkt) {
+    public boolean updateCosts(){
+	boolean costChange = false;
+	for( int myIndex = 0; myIndex < RouterSimulator.NUM_NODES; myIndex++){
+	    if(myIndex == myID){
+		continue;
+	    }
+	    else{
+		for( int i = 0; i < RouterSimulator.NUM_NODES; i++ ){ // One Node (not this node)
+		    if(i == myID) continue;
+		    for( int j = 0; j < RouterSimulator.NUM_NODES; j++ ){ // Another node (not this node)
+			if( j == i || j == myID ){ continue; }
+
+			int mini = Math.min( this.costs[i][myID]/*Node to this node*/ + this.costs[i][myIndex]/*Node to target node*/,
+					     this.costs[j][myID]/*Node to this node*/ + this.costs[j][myIndex]/*Node to target node*/ );
+
+			
+			int oldvalue = costs[myID][myIndex];
+			if(mini < myCost[myIndex]){
+			    costs[myID][myIndex] = mini;
+			}
+			else{
+			    costs[myID][myIndex] = myCost[myIndex];
+			}
+
+			if(oldvalue != costs[myID][myIndex]){
+			    costChange = true;
+			}
+		    }
+		}
+	    }
+	}
+	return costChange;
+    }
+
+    // Called by the simulator (executed) when a node receives an update from one of its neighbors
+    public void recvUpdate(RouterPacket pkt) {
 
       if(pkt.destid != this.myID){
 	  return;
       }
 
       System.arraycopy(pkt.mincost, 0, costs[pkt.sourceid], 0, RouterSimulator.NUM_NODES);
-      
-      for( int myIndex = 0; myIndex < RouterSimulator.NUM_NODES; myIndex++){
-	  if(myIndex == myID){
-	      continue;
-	  }
-	  else{
-	      for( int i = 0; i < RouterSimulator.NUM_NODES; i++ ){ // One Node (not this node)
-		  if(i == myID) continue;
-		  for( int j = 0; j < RouterSimulator.NUM_NODES; j++ ){ // Another node (not this node)
-		      if( j == i || i == myID ){ continue; }
 
-		      int mini = Math.min( this.costs[i][myID]/*Node to this node*/ + this.costs[i][myIndex]/*Node to target node*/,
-					  this.costs[j][myID]/*Node to this node*/ + this.costs[j][myIndex]/*Node to target node*/ );
-
-		      if(mini < costs[myID][myIndex]){
-			  costs[myID][myIndex] = mini;
-		      }
-		  }
-	      }
-	  }
+      if(updateCosts() == true){
+	  sendAllUpdate();
       }
   }
   
+    private void sendAllUpdate(){
+	// Send nodes dist-vector to neighbors.
+	for( int nodeID = 0 ; nodeID < RouterSimulator.NUM_NODES; ++nodeID ){
+	    if( myID != nodeID && myCost[nodeID] != RouterSimulator.INFINITY ){
+		RouterPacket newPkt = new RouterPacket(this.myID, nodeID, this.costs[myID]);
+		sendUpdate(newPkt);
+	    }
+	}
+    }
 
   //--------------------------------------------------
   private void sendUpdate(RouterPacket pkt) {
-      // Send yourself as infinity
-    sim.toLayer2(pkt);
+     
+      sim.toLayer2(pkt);
   }
   
 
@@ -78,25 +100,45 @@ public class RouterNode {
 	  myGUI.println("Current table for " + myID +
 			"  at time " + sim.getClocktime());
 	  
-	  myGUI.print(String.format("%-2s", ""));
+	  
+	  myGUI.println("\nDistancetable:");
+	  myGUI.print(String.format( "%7s %4s", "Dst", "|"));
 	  for(int xtitle = 0; xtitle < RouterSimulator.NUM_NODES; ++xtitle){
-	      myGUI.print( String.format( "%4s", (char)(xtitle+65) ) );
+	      myGUI.print( String.format( "%6s", xtitle ) );
 	  }
-	  myGUI.println();
+	  myGUI.println("\n--------------------------------------------------------");
 
 	  for(int col = 0; col < RouterSimulator.NUM_NODES; col++){
-	      myGUI.print( String.format( "%1s", (char)(col+65) ) );
+	      if( /*col == myID || */ myCost[col] == RouterSimulator.INFINITY ) continue;
+	      myGUI.print( String.format( "%1s %3s %3s", "nbr" , col, "|" ) );
 	      for(int row = 0; row < RouterSimulator.NUM_NODES; row++){
-		  myGUI.print( String.format( "%4s", this.costs[col][row] ) );
+		  myGUI.print( String.format( "%6s", this.costs[col][row] ) );
 	      }
 	      myGUI.println();
 	  }
+
+	  myGUI.println("\nOur distance vector and routes:");
+	  myGUI.print(String.format( "%7s %4s", "Dst", "|"));
+	  for(int xtitle = 0; xtitle < RouterSimulator.NUM_NODES; ++xtitle){
+	      myGUI.print( String.format( "%6s", xtitle ) );
+	  }
+	  myGUI.println("\n--------------------------------------------------------");
+
+	  myGUI.print( String.format( "%7s %3s", "Cost", "|" ) );
+	  for(int row = 0; row < RouterSimulator.NUM_NODES; row++){
+	      myGUI.print( String.format( "%6s", this.myCost[row] ) );
+	  }
+
+	  myGUI.println();
+
 	 
   }
 
   // called by the simulator (executed) when the cost of link of a node is about to change
   public void updateLinkCost(int dest, int newcost) {
-
+      myCost[dest] = newcost;
+      costs[dest][myID] = newcost;
+      updateCosts();
   }
 
 }
